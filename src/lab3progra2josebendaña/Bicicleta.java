a2f1d16ce3aa0657cf716446b0fb1fd9521a6420
JJBR2662 <bayernmunich2662@gmail.com>
2023-02-03 15:33:28 -0600

    Se crea la parte del CRUD de vehiculos ya que se crea el codigo encargado de verficar si es de 2 o 4 ruedas

--- src/lab3progra2josebendaña/Bicicleta.java
+++ src/lab3progra2josebendaña/Bicicleta.java
@@ -4,14 +4,16 @@ package lab3progra2josebendaña;
 public class Bicicleta extends Vehiculo{
     private String desc;
     private double radiorueda;
+    private String tipo;
 
     public Bicicleta() {
     }
 
-    public Bicicleta(String desc, double radiorueda, String color, String marca, String modelo, int anio, double precio, int cantllantas) {
+    public Bicicleta(String desc, double radiorueda, String color, String marca, String modelo, int anio, double precio, int cantllantas, String tipo) {
         super(color, marca, modelo, anio, precio, cantllantas);
         this.desc = desc;
         this.radiorueda = radiorueda;
+        this.tipo=tipo;
     }
 
     public String getDesc() {
@@ -30,9 +32,17 @@ public class Bicicleta extends Vehiculo{
         this.radiorueda = radiorueda;
     }
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
     @Override
     public String toString() {
-        return super.toString()+" Bicicleta{" + "desc=" + desc + ", radiorueda=" + radiorueda + '}';
+        return super.toString()+" Bicicleta{" + "desc=" + desc + ", radiorueda=" + radiorueda + ", tipo=" + tipo + '}';
     }
     
     
--- src/lab3progra2josebendaña/Lab3Progra2JoseBendaña.java
+++ src/lab3progra2josebendaña/Lab3Progra2JoseBendaña.java
@@ -9,6 +9,7 @@ public class Lab3Progra2JoseBendaña {
         Scanner enteros = new Scanner(System.in);
         Scanner orac = new Scanner(System.in);
         Scanner dobles = new Scanner(System.in);
+        Scanner car = new Scanner(System.in);
         // clases: Concesionaria, Clientes, Vehiculo, Carro, Camion de carga, bus, motocicleta, bici
         ArrayList<Concesionaria> concesionarias = new ArrayList();
         ArrayList<Cliente> clientes = new ArrayList();
@@ -50,21 +51,21 @@ public class Lab3Progra2JoseBendaña {
                                     System.out.println("Esta lista esta vacia");
                                 } else {
                                     int op;
-                                    do{
+                                    do {
                                         for (Cliente f : clientes) {
                                             System.out.println(clientes.indexOf(f) + "- " + f);
                                         }
                                         System.out.println("Ingrese que cliente quiere eliminar: ");
                                         op = enteros.nextInt();
-                                        if (op>=clientes.size()||op<0) {
+                                        if (op >= clientes.size() || op < 0) {
                                             System.out.println("Opcion no valida, ingrese una correcta");
                                         }
-                                    }while(op>=clientes.size()||op<0);
+                                    } while (op >= clientes.size() || op < 0);
                                     clientes.remove(op);
                                     System.out.println("Se ha borrado correctamente");
                                 }
                                 break;
-                                
+
                             default:
                                 System.out.println("Opcion no valida, ingrese de nuevo");
                                 break;
@@ -72,7 +73,110 @@ public class Lab3Progra2JoseBendaña {
                     } while (client > 2 || client < 1);
                     break;
                 case 3:
+                    int cual;
+                    do {
+                        System.out.println("""
+                               1- Crear Vehiculo
+                               2- Modificar Vehiculo
+                               3- Eliminar Vehiculo""");
+                        System.out.print("Ingrese una opcion: ");
+                        cual = enteros.nextInt();
+                        switch (cual) {
+                            case 1:
+                                if (concesionarias.isEmpty()) {
+                                    System.out.println("No se puede crear carro, no hay donde tirarlo");
+                                }else{
+                                    System.out.print("Ingrese el color del vehiculo: ");
+                                    String color = orac.nextLine();
+                                    System.out.print("Ingrese la marca del vehiculo: ");
+                                    String marca = orac.nextLine();
+                                    System.out.print("Ingrese el modelo del vehiculo: ");
+                                    String modelo = orac.nextLine();
+                                    System.out.print("Ingrese el anio de fabricacion: ");
+                                    int anio = enteros.nextInt();
+                                    System.out.print("Ingrese el precio del vehiculo: ");
+                                    double precio = dobles.nextDouble();
+                                    int cantllantas;
+                                    do{
+                                        System.out.print("Ingrese la cantidad de llantas [2 o 4]: ");
+                                        cantllantas = enteros.nextInt();
+                                        if (cantllantas>4||cantllantas==3||cantllantas<2) {
+                                            System.out.println("Cantidad de llantas no valida");
+                                        }
+                                    }while(cantllantas>4||cantllantas==3||cantllantas<2);
+                                    if (cantllantas==2){
+                                        int dos;
+                                        do{
+                                            System.out.println("""
+                                                               1- Motocicleta
+                                                               2- Bicicleta""");
+                                            System.out.print("Ingrese que vehiculo de dos llantas desea crear: ");
+                                            dos = enteros.nextInt();
+                                            switch(dos){
+                                                case 1:
+                                                    System.out.print("Ingrese el desplazamiento del motor: ");
+                                                    String desp = orac.nextLine();
+                                                    char resp;
+                                                    do{
+                                                        System.out.print("Es electrica [s/n]? ");
+                                                        resp = car.next().charAt(0);
+                                                        if (resp!='s'||resp!='S'||resp!='n'||resp!='N') {
+                                                            System.out.println("Opcion no valida, intente de nuevo");
+                                                        }
+                                                    }while(resp!='s'||resp!='S'||resp!='n'||resp!='N');
+                                                    boolean seraelectrica;
+                                                    if (resp=='s'||resp=='S'){
+                                                        seraelectrica = true;
+                                                    }else{
+                                                        seraelectrica = false;
+                                                    }
+                                                    vehiculos.add(new Motocicleta(desp, seraelectrica, color, marca, modelo, anio, precio, cantllantas));
+                                                    System.out.println("Se ha agregado la motocicleta perfectamente");
+                                                    break;
+                                                case 2:
+                                                    System.out.print("Ingrese la descrpcion de la bicicleta: ");
+                                                    String desc = orac.nextLine();
+                                                    System.out.print("Ingrese el radio de la rueda: ");
+                                                    double radio = dobles.nextDouble();
+                                                    int cual2;
+                                                    do{
+                                                        System.out.println("""
+                                                                           1- BMX
+                                                                           2- De Calle""");
+                                                        System.out.print("Ingrese el tipo de bicicleta: ");
+                                                        cual2 = enteros.nextInt();
+                                                        if (cual2>2||cual2<1) {
+                                                            System.out.println("Opcionno valida, ingrese una correcta");
+                                                        }
+                                                    }while(cual2>2||cual2<1);
+                                                    String tipo;
+                                                    if (cual2==1) {
+                                                        tipo = "BMX";
+                                                    }else{
+                                                        tipo = "De calle";
+                                                    }
+                                                    vehiculos.add(new Bicicleta(desc, radio, color, marca, modelo, anio, precio, cantllantas, tipo));
+                                                    System.out.println("Se ha agregado la bicicleta perfectamente");
+                                                    break;
+                                                default:
+                                                    System.out.println("Opcion no valida, ingrese de nuevo");
+                                                    break;
+                                            }
+                                        }while(dos>2||dos<1);
+                                    }
+                                }
+                                break;
+                            case 2:
 
+                                break;
+                            case 3:
+
+                                break;
+                            default:
+                                System.out.println("Opcion no valida, ingrese de nuevo");
+                                break;
+                        }
+                    } while (opcion > 3 || opcion < 1);
                     break;
                 case 4:
 
